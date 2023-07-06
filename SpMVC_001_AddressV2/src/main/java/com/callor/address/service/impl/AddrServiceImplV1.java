17738d699a0e1abbf331f48d3ea68b6b72e09cf2
callor <callor@callor.com>
2023-07-06 11:20:43 +0900

    todoList

--- SpMVC_001_AddressV2/src/main/java/com/callor/address/service/impl/AddrServiceImplV1.java
+++ SpMVC_001_AddressV2/src/main/java/com/callor/address/service/impl/AddrServiceImplV1.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.callor.address.dao.AddrDao;
 import com.callor.address.dao.BuyerDao;
@@ -88,6 +89,7 @@ public class AddrServiceImplV1 implements AddrService {
 		return addrDao.selectAll();
 	}
 
+	@Transactional
 	@Override
 	public AddrDto findById(String id) {
 		
