df08964d6be21a31ea0c54279e7773039025d0d2
callor <callor@callor.com>
2023-07-19 13:43:00 +0900

    thyemleaf

--- SpMVC_004_MyCarV2/src/main/webapp/static/js/main_input.js
+++ SpMVC_004_MyCarV2/src/main/webapp/static/js/main_input.js
@@ -1,32 +1,21 @@
-const INPUT_INDEX = {
-  C_CARNUM: 0,
-  C_QTY: 1,
-  C_USERNAME: 2,
-  C_SDATE: 3,
-  C_STIME: 4,
-};
 document.addEventListener("DOMContentLoaded", () => {
-  const formEnterHandler = (e) => {
-    const target = e.target;
-    //  화면에서 Enter(13) 가 눌리고
-    // focus() 대상이 INPUT tag 이면
-    if (e.keyCode === 13 && target.tagName === "INPUT") {
-      // focus() 가 있는 input tag 의 name 속성을 getter
-      // 대문자로 변경하라
-      const input_name = target["name"].toUpperCase();
-      // focus() 가 있는 input tag 가 몇번째 tag 이냐
-      const input_index_num = INPUT_INDEX[input_name];
+  const input_carnum = document.querySelector("#c_carnum");
+  const input_sdate = document.querySelector("#c_sdate");
+  const input_stime = document.querySelector("#c_stime");
+  const input_qty = document.querySelector("#c_qty");
+  const input_skm = document.querySelector("#c_skm");
 
-      // 현재 focus() 가 있는 input tag 가 마지막 input 이
-      // 아니면
-      if (input_index_num < Object.keys(INPUT_INDEX).length - 1) {
-        // 다음번 input 으로 focus를 이동하라
-        document
-          .querySelectorAll("input")
-          [input_index_num + 1].focus();
-      }
-    }
-  };
-  document.addEventListener("keydown", formEnterHandler);
-  document.querySelector("input[name='c_carnum']").focus();
+  const err_carnum = document.querySelector("div.error.c_carnum");
+  const err_qty = document.querySelector("div.error.c_qty");
+  const err_sdate = document.querySelector("div.error.c_sdate");
+  const err_stime = document.querySelector("div.error.c_stime");
+  const err_skm = document.querySelector("div.error.c_skm");
+
+  // input box 에 focus() 를 설정하고 입력된 Text 가 있으면
+  // Text 전체를 블럭으로 설정하라
+  input_carnum.select();
+
+  input_carnum?.addEventListener("blur", (e) => {
+    err_message(err_carnum, e.target, "* 차량번호를 입력하세요");
+  });
 });
--- SpMVC_004_MyCarV2/src/main/webapp/static/js/main_input_validation.js
+++ SpMVC_004_MyCarV2/src/main/webapp/static/js/main_input_validation.js
@@ -1,40 +1,15 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const input_carnum = document.querySelector(
-    "input[name='c_carnum']"
-  );
-  const input_skm = document.querySelector("input[name='c_skm']");
-  const input_qty = document.querySelector("input[name='c_qty']");
+const err_clear = () => {
+  const err_box_list = document.querySelectorAll("div.error");
+  for (let box of err_box_list) {
+    box.classList.remove("on");
+  }
+};
 
-  input_carnum?.addEventListener("blur", (e) => {
-    const value = e.target.value;
-    const error_carnum = document.querySelector("div.error.c_carnum");
-
-    if (!value) {
-      error_carnum?.classList.add("on");
-      error_carnum.innerHTML = "* 차량번호는 반드시 입력하세요";
-      e.target.focus();
-      return false;
-    } else {
-      fetch(`${rootPath}/car_check?carnum=${value}`) // 요청
-        .then((response) => response.json()) // 응답 한 데이터중 JSON 만 추출
-        .then((result) => {
-          if (result?.c_carnum === "NOT") {
-            error_carnum.classList.add("on");
-            error_carnum.innerHTML = "* 없는 차량번호 입니다";
-          } else {
-            input_skm.value = result.c_skm;
-          }
-        });
-    }
-  });
-  input_qty?.addEventListener("blur", (e) => {
-    const value = e.target.value;
-    if (!value) {
-      const error_qty = document.querySelector("div.error.c_qty");
-      error_qty.classList.add("on");
-      error_qty.innerHTML = "* 업무구분은 반드시 입력해야 합니다";
-      e.target.focus();
-      return false;
-    }
-  });
-});
+const err_message = (err_box, e, message) => {
+  const value = e.value;
+  if (!value) {
+    err_clear();
+    err_box.classList.add("on");
+    err_box.innerHTML = message;
+  }
+};
