c64e079975e17e441ff969cecb0c333857b8735a
callor <callor@callor.com>
2023-07-30 09:32:09 +0900

    files

--- SpMVC_006_Filels/src/main/java/com/callor/file/model/BBsDto.java
+++ SpMVC_006_Filels/src/main/java/com/callor/file/model/BBsDto.java
@@ -1,5 +1,12 @@
 package com.callor.file.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 @Getter
 @Setter
 @ToString
