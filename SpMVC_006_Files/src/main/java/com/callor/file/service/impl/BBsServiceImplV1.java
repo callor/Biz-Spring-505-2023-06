f46ad93e625254a7c45f00835c36eb87b639d2f0
callor <callor@callor.com>
2023-07-31 14:39:22 +0900

    file

--- SpMVC_006_Files/src/main/java/com/callor/file/service/impl/BBsServiceImplV1.java
+++ SpMVC_006_Files/src/main/java/com/callor/file/service/impl/BBsServiceImplV1.java
@@ -12,6 +12,9 @@ import com.callor.file.model.BBsDto;
 import com.callor.file.service.BBsService;
 import com.callor.file.service.FileService;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class BBsServiceImplV1 implements BBsService{
 
@@ -78,25 +81,34 @@ public class BBsServiceImplV1 implements BBsService{
 		String upload_name = b_file.getOriginalFilename();
 		long b_seq = bbsDto.getB_seq();
 		
-		bbsDto = bbsDao.findById(b_seq);
-		String old_name = bbsDto.getB_origin_image();
+		BBsDto resultDto = bbsDao.findById(b_seq);
+		String old_name = resultDto.getB_origin_image();
 		
+		// 새로 업로드된 이미지가 있고,
+		// 원래 저장된 파일이름과 다르면
 		if( !upload_name.isBlank() && !upload_name.equals(old_name)) {
-			fileService.delete(bbsDto.getB_image());
+			// 원래 저장된(업로드된) 파일을 삭제
+			fileService.delete(resultDto.getB_image());
 			try {
+				// 새로운 파일을 업로드하기
 				upload_name = fileService.fileUp(b_file);
+				// 새로운 파일정보(파일이름, 업로드이름)  Dto 에 setting
 				bbsDto.setB_image(upload_name);
 				bbsDto.setB_origin_image(b_file.getOriginalFilename());
+				// Update 실행
 				bbsDao.update(bbsDto);
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+			// 새롭게 선택(update)된 파일이 없으면
 		} else {
+			// 원래 DB 의 저장된 이름을 다시 bbsDto 에 세팅
+			bbsDto.setB_image(resultDto.getB_image());
+			bbsDto.setB_origin_image(resultDto.getB_origin_image());
+			// update 실행
 			bbsDao.update(bbsDto);
 		}
-		
-		
 		return 0;
 	}
 	
